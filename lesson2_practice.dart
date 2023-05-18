6bf47493f6560813cb86a93e039c154a0c02acea
sezimka123 <erali.sezim@gmail.com>
2023-05-18 16:06:59 +0600

    first commit

---
+++ lesson2_practice.dart
@@ -0,0 +1,61 @@
+void main() {
+  num age = 22;
+  num day = 2;
+  num degrees = 22;
+  num hours = 19;
+  num minutes = 42.5;
+
+  int a = 12;
+  int b = -2;
+  int c = 345;
+  int d = 0;
+  int e = -46;
+
+  double f = 22.35;
+  double g = 15.04;
+  double h = 56.7;
+  double i = -33.3333;
+  double j = 0.24;
+
+  String name = "Sezim";
+  String dayOfTheWeek = "Tuesday";
+  String status = "student";
+  String city = "Bishkek";
+  String topicOfTheLesson = "Data types";
+
+  List week = ["monday", "tuesday", "wednesday"];
+  List months = ["may", "june", "august"];
+  List arrayList = [1, 4, true, "Sezim", 10.001];
+  List<bool> answers = [true, false];
+  List<int> ages = [22, 19, 25, 28];
+
+  Map team = {"numberOfMembers": 6, "nameOfTheTeam": 'BTS'};
+  Map school = {"teacher": "Irina", "student": "Sezim"};
+  Map lesson = {"subject": "Data types", "time": 2};
+  Map student = {"gender": "female", "age": 23};
+  Map meal = {"breakfast": "sandwich", "lunch": "pasta"};
+
+  String smile = '\u{1F604}';
+  String sad = '\u{1F622}';
+  String angry = '\u{1F621}';
+  String monkey = '\u{1F648}';
+  String devil = '\u{1F608}';
+
+  final k;
+  k = 99;
+  final l = "Cool";
+  final m = true;
+  final n = 10.9;
+  final o = -3;
+
+  const p = 66;
+
+
+
+
+
+
+
+
+
+}
